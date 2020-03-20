475ebeb75a40f6870a2086ae26ec2547cade0d11
Shivam Kumar <shivam123425@gmail.com>
2020-03-20 21:36:34 +0530

    Add ResponsiveFlex

--- components/Flex/index.js
+++ components/Flex/index.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 
-export default styled.div`
+const Flex = styled.div`
   display: flex;
   flex-wrap: ${({ wrap }) => wrap || 'nowrap'};
   justify-content: ${({ justify }) => justify || 'initial'};
   align-items: ${({ align }) => align || 'initial'};
 `;
+
+export const ResponsiveFlex = styled(Flex)`
+  flex-wrap: wrap;
+  @media only screen and (max-width: ${({ width }) => width ? width : '768px'}) {
+    flex-direction: column;
+  }
+`; 
+
+export default Flex;
