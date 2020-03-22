94d534cc29d2710dd21033792ad6a83866402feb
Shivam Kumar <shivam123425@gmail.com>
2020-03-22 22:10:57 +0530

    Use participationTokens stored in redux or fetch if necessary

--- modules/Event/components/EventAction.js
+++ modules/Event/components/EventAction.js
@@ -1,21 +1,56 @@
+import { useEffect, useState, useCallback } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { createStructuredSelector } from "reselect";
 import { pick } from "ramda";
+import cogoToast from "cogo-toast";
 import Button from "components/Button";
-import {
-  makeSelectUser,
-  makeSelectParticipationTokens
-} from "modules/Auth/redux/selectors";
+import Spinner from "components/Spinner";
+import { makeSelectParticipationTokens } from "modules/Auth/redux/selectors";
 import * as authActions from "modules/Auth/redux/actions";
 import theme from "theme";
+import request from "lib/request";
+import config from "config/env";
 
-const EventAction = ({ event, user, participationTokens }) => {
-  return <Button backgroundColor={theme.primaryGreen}>Pariticipate</Button>;
+const EventAction = ({
+  event: { eventId },
+  participationTokens,
+  fetchTokenSuccess
+}) => {
+  const [loading, setLoading] = useState(false);
+  const fetchToken = useCallback(async () => {
+    try {
+      if (!participationTokens[eventId]) {
+        setLoading(true);
+        const token = await request(
+          `${config.apiUrl}/token?eventId=${eventId}`
+        );
+        setLoading(false);
+        fetchTokenSuccess(token);
+      }
+    } catch (error) {
+      cogoToast.error(
+        "Some error occurred while fetching the participation token. Please try again later!"
+      );
+      setLoading(false);
+    }
+  }, [eventId, participationTokens]);
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+  return (
+    <Button backgroundColor={theme.primaryGreen}>
+      {loading ? (
+        <Spinner type="TailSpin" color="#fff" height={20} width={20} />
+      ) : (
+        "Participate"
+      )}
+    </Button>
+  );
 };
 
 const mapStateToProps = createStructuredSelector({
-  user: makeSelectUser(),
   participationTokens: makeSelectParticipationTokens()
 });
 
