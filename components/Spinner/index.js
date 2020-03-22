127dd9bf2ae3bc55f61eb7fe0d61e862536ba7eb
Shivam Kumar <shivam123425@gmail.com>
2020-03-22 22:10:10 +0530

    seperate out default values and spread props

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,5 +1,10 @@
 import Loader from "react-loader-spinner";
 
-export default props => (
-  <Loader type="Watch" color="#00BFFF" height={100} width={100} />
-);
+const defaultProps = {
+  type: "Watch",
+  color: "#00BFFF",
+  height: 100,
+  width: 100
+};
+
+export default props => <Loader {...defaultProps} {...props} />;
