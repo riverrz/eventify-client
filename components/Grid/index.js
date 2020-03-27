59e75526a2742a4091c594be0050438024713f0e
Shivam Kumar <shivam123425@gmail.com>
2020-03-27 15:22:36 +0530

    Full auto-fill instead

--- components/Grid/index.js
+++ components/Grid/index.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export default styled.div`
   display: grid;
   grid-gap: 1.5rem;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
 `;
