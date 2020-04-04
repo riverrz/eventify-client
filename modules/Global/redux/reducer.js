ff339552295c7a0f5aed387b38618ca87471027c
Shivam Kumar <shivam123425@gmail.com>
2020-04-04 21:41:33 +0530

    Add logic for modals

--- modules/Global/redux/reducer.js
+++ modules/Global/redux/reducer.js
@@ -1,21 +1,25 @@
-import { produce } from 'immer';
-import * as actionTypes from './constants';
+import { produce } from "immer";
+import nanoid from "nanoid";
+import {} from "ramda";
+import * as actionTypes from "./constants";
 
 const initialState = {
-  modals: {}
-}
+  modals: {},
+};
 
-export default function(state = initialState, { type, payload }) {
-  return produce(state, draft => {
-    switch(type) {
+export default function (state = initialState, { type, payload }) {
+  return produce(state, (draft) => {
+    switch (type) {
       case actionTypes.OPEN_MODAL: {
-        
+        const id = nanoid();
+        draft.modals[id] = { ...payload, id };
         break;
       }
       case actionTypes.CLOSE_MODAL: {
-
+        const { id } = payload;
+        draft.modals = omit([id], draft.modals);
         break;
       }
     }
-  })
+  });
 }
