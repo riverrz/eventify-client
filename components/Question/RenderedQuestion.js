dfc37e5983e7d360b357dadb41704a78d6fcab4c
Shivam Kumar <shivam123425@gmail.com>
2020-04-28 18:48:27 +0530

    Use Formik and manage answers

--- components/Question/RenderedQuestion.js
+++ components/Question/RenderedQuestion.js
@@ -1,31 +1,77 @@
 import styled from "styled-components";
 import { Formik, Field } from "formik";
+import Flex from "components/Flex";
+import Button from "components/Button";
+import { Checkbox, CheckboxGroup } from "components/Checkbox";
 
-function RenderedQuestion({ className, data, qNo }) {
+function RenderedQuestion({
+  className,
+  data,
+  step,
+  move,
+  save,
+  initialValues = {},
+}) {
   const { question, type, choices } = data;
+
   return (
     <div className={className}>
       <div
         className="question"
-        dangerouslySetInnerHTML={{ __html: `Q${qNo}. ${question}` }}
+        dangerouslySetInnerHTML={{ __html: `Q${step}. ${question}` }}
       />
-      <Choices type={type} choices={choices} />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          save({ type, step, values });
+        }}
+        enableReinitialize
+      >
+        {(formikProps) => (
+          <form onSubmit={formikProps.handleSubmit} className="form">
+            <Choices type={type} choices={choices} formikProps={formikProps} />
+            <Button type="submit">Save</Button>
+          </form>
+        )}
+      </Formik>
+      <Flex justify="space-around" className="actions">
+        {step > 1 && <Button onClick={() => move(step - 1)}>Back</Button>}
+        <Button
+          onClick={() => {
+            move(step + 1);
+          }}
+        >
+          Next
+        </Button>
+      </Flex>
     </div>
   );
 }
 
-function Choices({ type, choices }) {
-  if (type == "Text field") {
-    return <input type="text" />;
-  } else {
-    return choices.map((choice) => {
+function Choices({ type, choices, formikProps }) {
+  switch (type) {
+    case "Text field": {
+      return <Field name="answer" required placeholer="Enter your answer" />;
+    }
+    default: {
+      const { values, setFieldValue } = formikProps;
       return (
-        <>
-          <input type="checkbox" name="" id="" />
-          <label htmlFor="">{choice}</label>
-        </>
+        <CheckboxGroup
+          id="answer"
+          value={values.answer || []}
+          onChange={setFieldValue}
+        >
+          {choices.map((choice, i) => (
+            <Field
+              component={Checkbox}
+              name="answer"
+              id={`checkbox${i + 1}`}
+              label={choice}
+            />
+          ))}
+        </CheckboxGroup>
       );
-    });
+    }
   }
 }
 
@@ -34,6 +80,9 @@ export default styled(RenderedQuestion)`
   padding: 1rem;
   background-color: #eee;
   .question {
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
+  }
+  .actions {
+    margin: 1rem 0;
   }
 `;
--- modules/LiveEvent/components/EventEngine/ContentfulEngine.js
+++ modules/LiveEvent/components/EventEngine/ContentfulEngine.js
@@ -1,28 +1,62 @@
 import styled from "styled-components";
 import { Component } from "react";
+import { pick } from "ramda";
 import Timer from "components/Timer";
 import RenderedQuestion from "components/Question/RenderedQuestion";
 
 class ContentfulEngine extends Component {
   state = {
     data: {},
+    step: 1,
   };
+
   finishHandler = () => {
     // event finished
-    this.props.endEvent();
+    this.props.endEvent(this.state.data);
+  };
+  move = (step) => {
+    this.setState({
+      step,
+    });
+  };
+  componentDidUpdate() {
+    if (this.state.step > Object.keys(this.props.blob).length) {
+      this.props.endEvent(this.state.data);
+    }
+  }
+  save = ({ values, step, type }) => {
+    let newValues;
+    if (type === "Text field") {
+      newValues = { values, type };
+    } else {
+      newValues = { ...values, type };
+    }
+    const data = { ...this.state.data, [step]: newValues };
+    this.setState({
+      data,
+    });
   };
   render() {
     const { className, blob } = this.props;
+    const { step } = this.state;
+    const totalQuestions = blob && Object.keys(blob).length;
     return (
       <main className={className}>
         <div className="timer">
           <Timer onFinish={this.finishHandler} />
         </div>
         <div className="container">
-          {blob &&
-            Object.entries(blob).map(([qNo, meta]) => {
-              return <RenderedQuestion key={qNo} data={meta} qNo={qNo} />;
-            })}
+          {blob && step <= totalQuestions && (
+            <RenderedQuestion
+              data={blob[step]}
+              initialValues={
+                this.state.data[step] && pick(["answer"], this.state.data[step])
+              }
+              move={this.move}
+              step={step}
+              save={this.save}
+            />
+          )}
         </div>
       </main>
     );
@@ -32,6 +66,7 @@ class ContentfulEngine extends Component {
 export default styled(ContentfulEngine)`
   position: relative;
   height: 100%;
+  padding-top: 2rem;
   .timer {
     position: absolute;
     top: 0;
