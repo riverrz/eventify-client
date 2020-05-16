852a2037543f779114b971c07782edd688e28d2d
Shivam Kumar <shivam123425@gmail.com>
2020-05-17 00:06:36 +0530

    Improve spacing and layout

--- components/Choices/index.js
+++ components/Choices/index.js
@@ -1,11 +1,23 @@
 import Checkbox from "components/Checkbox";
 import Radio from "components/Radio";
+import Flex from "components/Flex";
 
-export default ({ type, choices = [] }) =>
-  choices.map((choice, index) => {
-    if (type === "Single Choice") {
-      return <Radio name="answer" value={choice} key={index} />;
-    } else {
-      return <Checkbox name="answer" value={choice} key={index} />;
-    }
-  });
+export default ({ type, choices = [] }) => (
+  <Flex direction="column">
+    {choices.map((choice, index) => {
+      if (type === "Single Choice") {
+        return (
+          <div style={{ margin: "0.5rem" }}>
+            <Radio name="answer" value={choice} key={index} />
+          </div>
+        );
+      } else {
+        return (
+          <div style={{ margin: "0.5rem" }}>
+            <Checkbox name="answer" value={choice} key={index} />
+          </div>
+        );
+      }
+    })}
+  </Flex>
+);
