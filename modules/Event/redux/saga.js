ccc22c9cbbd2d53d637869e63e570a125bf5bf7a
Shivam Kumar <shivam123425@gmail.com>
2020-05-20 12:50:51 +0530

    Fetch user again after participationRequest is success

--- modules/Event/redux/saga.js
+++ modules/Event/redux/saga.js
@@ -1,6 +1,7 @@
 import { takeEvery, call } from "redux-saga/effects";
 import cogoToast from "cogo-toast";
 import * as actionTypes from "./constants";
+import { fetchUserRequest } from "modules/Auth/redux/actions";
 import config from "config/env";
 import request from "lib/request";
 
@@ -11,17 +12,18 @@ function* participateRequestSaga({ payload }) {
     yield call(request, requestUrl, {
       method: "POST",
       body: JSON.stringify({
-        participationToken: token
-      })
+        participationToken: token,
+      }),
     });
     cogoToast.success(
       `You have successfully registered for the event ${title}`
     );
+    yield put(fetchUserRequest());
   } catch (error) {
     cogoToast.error(error.message);
   }
 }
 
 export default [
-  takeEvery(actionTypes.PARTICIPATE_REQUEST, participateRequestSaga)
+  takeEvery(actionTypes.PARTICIPATE_REQUEST, participateRequestSaga),
 ];
